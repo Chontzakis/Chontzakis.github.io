29a76db73604c61675ec73c145b96a8cc81dda12
chontzakis <55055493+Chontzakis@users.noreply.github.com>
2025-10-15 17:12:49 +0300

    Update background_home.js

--- background_home.js
+++ background_home.js
@@ -14,7 +14,7 @@ function resizeCanvas() {
 
     // Adjust layers for small screens
     if (W < 500) {
-    layers = [4, 8, 8, 2];
+    layers = [2, 8, 8, 2];
     }
 
     canvas.width = W * dpr;
